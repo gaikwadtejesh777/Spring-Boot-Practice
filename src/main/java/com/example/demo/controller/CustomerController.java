3ecf4a7e128819ac9b2f2b96b035e904ef96b12a
TejeshGaikwad <47993316+gaikwadtejesh777@users.noreply.github.com>
2021-08-09 10:16:24 +0530

    Update CustomerController.java

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -14,42 +14,23 @@ public class CustomerController {
     @Resource(name = "customerService")
     private CustomerService customerService;
 
-    /**
-     * <p>Get all customer data in the system.For production system you many want to use
-     * pagination.</p>
-     * @return List<CustomerData>
-     */
     @GetMapping
     public List < CustomerData > getCustomers() {
         return customerService.getAllCustomers();
     }
 
-    /**
-     * Method to get the customer data based on the ID.
-     * @param id
-     * @return CustomerData
-     */
+  
     @GetMapping("/customer/{id}")
     public CustomerData getCustomer(@PathVariable Long id) {
         return customerService.getCustomerById(id);
     }
 
-    /**
-     * Post request to create customer information int the system.
-     * @param customerData
-     * @return
-     */
     @PostMapping("/customer")
     public CustomerData saveCustomer(final @RequestBody CustomerData customerData) {
         return customerService.saveCustomer(customerData);
     }
 
-    /**
-     * <p>Delete customer from the system based on the ID. The method mapping is like the getCustomer with difference of
-     * @DeleteMapping and @GetMapping</p>
-     * @param id
-     * @return
-     */
+   
     @DeleteMapping("/customer/{id}")
     public Boolean deleteCustomer(@PathVariable Long id) {
         return customerService.deleteCustomer(id);
